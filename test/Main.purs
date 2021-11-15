2d07d1b83dbb5ec87b0efb35ef956748915b4708
oldfartdeveloper <scottnelsonsmith@gmail.com>
2021-11-15 14:28:49 -0800

    Removed test 'Features'

--- test/Main.purs
+++ test/Main.purs
@@ -18,11 +18,11 @@ main = launchAff_ $ runSpec [consoleReporter] do
         let isAwesome = true
         isAwesome `shouldEqual` true
       pending "feature complete"
-    describe "Features" do
-      it "runs in NodeJS" $ pure unit
-      it "runs in the browser" $ pure unit
-      it "supports streaming reporters" $ pure unit
-      it "supports async specs" do
-        res <- delay (Milliseconds 100.0) $> "Alligator"
-        res `shouldEqual` "Alligator"
-      it "is PureScript 0.12.x compatible" $ pure unit
+    -- describe "Features" do
+    --   it "runs in NodeJS" $ pure unit
+    --   it "runs in the browser" $ pure unit
+    --   it "supports streaming reporters" $ pure unit
+    --   it "supports async specs" do
+    --     res <- delay (Milliseconds 100.0) $> "Alligator"
+    --     res `shouldEqual` "Alligator"
+    --   it "is PureScript 0.12.x compatible" $ pure unit
